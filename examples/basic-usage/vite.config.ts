c57f10c283b01dcbe9a733a2cb8c2cfeeb940b00
Anılcan Metinyurt <igoodie@programmer.net>
2024-01-11 01:30:29 +0300

    feat: set up gh-pages for demo

--- examples/basic-usage/vite.config.ts
+++ examples/basic-usage/vite.config.ts
@@ -5,4 +5,5 @@ import richSvg from "vite-plugin-react-rich-svg";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), richSvg()],
+  base: "/vite-plugin-react-rich-svg/", // <- Here, so we can deploy this example to Github Pages
 });
