13e8692c17b070c66680cf43c303e34c83e169ca
Anılcan Metinyurt <igoodie@programmer.net>
2024-02-16 23:39:55 +0300

    feat: migrate styles to Tailwind

--- examples/basic-usage/src/App.tsx
+++ examples/basic-usage/src/App.tsx
@@ -16,74 +16,115 @@ import arrowIcon from "./assets/arrow.svg";
 
 function App() {
   return (
-    <main>
-      <h1>vite-plugin-react-rich-svg</h1>
-      <nav>
+    <main className="min-h-screen mb-16 mt-16">
+      <h1 className="flex justify-center items-center text-3xl font-bold tracking-wider p-6 text-teal-500">
+        vite-plugin-react-rich-svg
+      </h1>
+      <nav className="flex justify-center gap-2">
         <a href="https://github.com/iGoodie/vite-plugin-react-rich-svg">
-          <span>Github</span>
+          <span className="cursor-pointer underline text-teal-500 hover:text-teal-700">
+            Github
+          </span>
         </a>
         <span>•</span>
         <a href="https://www.npmjs.com/package/vite-plugin-react-rich-svg">
-          <span>Npm Page</span>
+          <span className="cursor-pointer underline text-teal-500 hover:text-teal-700">
+            Npm Page
+          </span>
         </a>
       </nav>
 
-      <div className="logo">
-        <h1>
+      <section className="max-w-[800px] mx-auto grid grid-cols-[auto,1fr] gap-x-[16px] gap-y-[8px] relative">
+        <h1 className="text-lg font-bold col-span-2">
           Raw - <em>*.svg?raw</em>
         </h1>
-        <img className="arrow" src={arrowIcon} />
-        <div dangerouslySetInnerHTML={{ __html: viteLogoRaw }} />
-        <code>
+        <img
+          className="absolute arrow left-[-45px] top-[70px]"
+          src={arrowIcon}
+        />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: viteLogoRaw,
+          }}
+        />
+
+        <code className="whitespace-pre overflow-auto flex bg-gray-800 p-8 text-white font-mono text-lg border border-gray-600 rounded-[5px] shadow-lg min-h-[150px]">
           export default "
           {viteLogoRaw.replaceAll('"', '\\"').replace(/\r?\n/g, "\\r\\n")}"
         </code>
-      </div>
+      </section>
 
-      <div className="logo">
-        <h1>
+      <div className="p-4" />
+
+      <section className="max-w-[800px] mx-auto grid grid-cols-[auto,1fr] gap-x-[16px] gap-y-[8px] relative">
+        <h1 className="text-lg font-bold col-span-2">
           Data URL - <em>*.svg?url</em>
         </h1>
-        <img className="arrow" src={arrowIcon} />
+        <img
+          className="absolute arrow left-[-45px] top-[70px]"
+          src={arrowIcon}
+        />
         <img src={viteLogoDataURL} alt="Vite logo" />
-        <code>
+
+        <code className="whitespace-pre overflow-auto flex bg-gray-800 p-8 text-white font-mono text-lg border border-gray-600 rounded-[5px] shadow-lg min-h-[150px]">
           export default "{viteLogoDataURL}"{"\n\n"}
         </code>
-      </div>
+      </section>
 
-      <div className="logo">
-        <h1>
+      <div className="p-4" />
+
+      <section className="max-w-[800px] mx-auto grid grid-cols-[auto,1fr] gap-x-[16px] gap-y-[8px] relative">
+        <h1 className="text-lg font-bold col-span-2">
           Data URL - <em>*.svg?base64</em>
         </h1>
-        <img className="arrow" src={arrowIcon} />
+        <img
+          className="absolute arrow left-[-45px] top-[70px]"
+          src={arrowIcon}
+        />{" "}
         <img
           src={`data:image/svg+xml;base64,${viteLogoBase64}`}
           alt="Vite logo"
         />
-        <code>
+        <code className="whitespace-pre overflow-auto flex bg-gray-800 p-8 text-white font-mono text-lg border border-gray-600 rounded-[5px] shadow-lg min-h-[150px]">
           export default "{viteLogoBase64}"{"\n\n"}
         </code>
-      </div>
+      </section>
 
-      <div className="logo">
-        <h1>
+      <div className="p-4" />
+
+      <section className="max-w-[800px] mx-auto grid grid-cols-[auto,1fr] gap-x-[16px] gap-y-[8px] relative">
+        <h1 className="text-lg font-bold col-span-2">
           Component - <em>*.svg?component</em>
         </h1>
-        <img className="arrow" src={arrowIcon} />
-        <ViteLogoComponent />
-        <code>
+        <img
+          className="absolute arrow left-[-45px] top-[70px]"
+          src={arrowIcon}
+        />{" "}
+        <img
+          src={`data:image/svg+xml;base64,${viteLogoBase64}`}
+          alt="Vite logo"
+        />
+        <code className="whitespace-pre overflow-auto flex bg-gray-800 p-8 text-white font-mono text-lg border border-gray-600 rounded-[5px] shadow-lg min-h-[150px]">
           export default {ViteLogoComponent.toString()}
           {"\n\n"}
         </code>
-      </div>
+      </section>
 
-      <div className="logo">
-        <h1>
+      <div className="p-4" />
+
+      <section className="max-w-[800px] mx-auto grid grid-cols-[auto,1fr] gap-x-[16px] gap-y-[8px] relative">
+        <h1 className="text-lg font-bold col-span-2">
           Default Handler - <em>*.svg</em>
         </h1>
-        <img className="arrow" src={arrowIcon} />
-        <img src={viteLogo} alt="Vite logo" />
-        <code>
+        <img
+          className="absolute arrow left-[-45px] top-[70px]"
+          src={arrowIcon}
+        />{" "}
+        <img
+          src={`data:image/svg+xml;base64,${viteLogoBase64}`}
+          alt="Vite logo"
+        />
+        <code className="whitespace-pre overflow-auto flex bg-gray-800 p-8 text-white font-mono text-lg border border-gray-600 rounded-[5px] shadow-lg min-h-[150px]">
           // This value depends on your vite config,
           {"\n"}
           // as this plugin passes, when svg import is missing a query
@@ -94,11 +135,11 @@ function App() {
               viteLogo.toString()}
           {"\n\n"}
         </code>
-      </div>
+      </section>
 
-      <hr />
+      <hr className="mt-4 mb-4 max-w-[100px] mx-auto" />
 
-      <div className="license">
+      <div className="flex flex-col justify-center items-center gap-[16px] py-4">
         <span>© 2024 Taha Anılcan Metinyurt (iGoodie)</span>
         <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
           <img
--- examples/basic-usage/tailwind.config.js
+++ examples/basic-usage/tailwind.config.js
@@ -1,1 +1,8 @@
-module.exports = {}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
