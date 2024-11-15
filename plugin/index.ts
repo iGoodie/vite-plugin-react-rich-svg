8a4ac6233171bc913d9530ff33d669c02745975e
Taha Anılcan Metinyurt <igoodie@programmer.net>
2024-11-15 21:29:09 +0300

    fix: asset cache persists after svg hmr
    Resolves #4

--- plugin/index.ts
+++ plugin/index.ts
@@ -1,35 +1,89 @@
 import { EsbuildTransformOptions, Plugin } from "vite";
 import type { Config as SvgrConfig } from "@svgr/core";
+import type { Config as SvgoConfig } from "svgo";
 
 import { resolveDataURI } from "./resolvers/dataURI";
 import { resolveRaw } from "./resolvers/raw";
 import { resolveReactComponent } from "plugin/resolvers/component";
 import { resolveBase64 } from "plugin/resolvers/base64";
 
-export default function (options?: {
+export interface PluginOptions {
   /**
    * If "include" predicate is present, only passing paths will be processed
    * @param path E.g "C:/path/to/my-svg.svg"
    * @returns Whether given path should be processed by the plugin or not
    */
   include?: (path: string) => boolean;
+
   /**
    * If "exclude" predicate is present, every path excluding passing paths will be processed
    * @param path E.g "C:/path/to/my-svg.svg"
    * @returns Whether given path should be discarded or processed by the plugin
    */
   exclude?: (path: string) => boolean;
+
+  /**
+   * Options used while resolving as "?raw"
+   */
+  rawLoaderOptions?: {
+    /**
+     * Enables SVGO optimizations while loading the SVG asset
+     * @default false
+     */
+    svgoEnabled?: boolean;
+
+    /**
+     * Options used while running SVGO optimizations on the original SVG asset
+     */
+    svgoConfig?: SvgoConfig;
+  };
+
+  /**
+   * Options used while resolving as "?component"
+   */
   componentLoaderOptions?: {
     /**
-     * Options used while running SVGR on the original svg code/asset
+     * Options used while running SVGR transform on the original svg asset
      */
     svgrConfig?: SvgrConfig;
     /**
-     * Options used to generate import code with given SVGR output
+     * Options used to generate JSX Component code with given SVGR output
      */
     esbuildConfig?: EsbuildTransformOptions;
   };
-}): Plugin {
+}
+
+export default function (options?: PluginOptions): Plugin {
+  const { componentLoaderOptions } = options ?? {};
+
+  interface HandlerParams {
+    id: string;
+    path: string;
+  }
+
+  const handlers = {
+    raw: ({ path }: HandlerParams) => {
+      return resolveRaw(path, options?.rawLoaderOptions);
+    },
+
+    url: ({ path }: HandlerParams) => {
+      return resolveDataURI(path);
+    },
+
+    base64: ({ path }: HandlerParams) => {
+      return resolveBase64(path);
+    },
+
+    component: ({ id, path }: HandlerParams) => {
+      return resolveReactComponent(
+        id,
+        path,
+        componentLoaderOptions?.svgrConfig,
+        componentLoaderOptions?.esbuildConfig
+      );
+    },
+  };
+
   function shouldProcessPath(path: string) {
     if (!path.endsWith(".svg")) {
       return false;
@@ -52,20 +106,27 @@ export default function (options?: {
     name: "vite-plugin-react-rich-svg",
     enforce: "pre",
 
-    async load(id) {
-      const [path, query] = id.split("?", 2);
+    async handleHotUpdate({ file, server }) {
+      if (file.endsWith(".svg")) {
+        const suffices = Object.keys(handlers)
+          .map((modifier) => "?" + modifier)
+          .concat("");
 
-      if (!shouldProcessPath(path)) {
-        return;
-      }
+        for (let suffix of suffices) {
+          const moduleId = file + suffix;
+          const module = server.moduleGraph.getModuleById(moduleId);
 
-      switch (query) {
-        case "raw":
-          return resolveRaw(path);
-        case "url":
-          return resolveDataURI(path);
-        case "base64":
-          return resolveBase64(path);
+          if (module) {
+            server.moduleGraph.invalidateModule(module);
+          } else {
+            console.error("Unknown module by id", moduleId);
+          }
+        }
+
+        server.hot.send({
+          type: "full-reload",
+          path: "*",
+        });
       }
     },
 
@@ -76,16 +137,9 @@ export default function (options?: {
         return;
       }
 
-      switch (query) {
-        case "component": {
-          const { componentLoaderOptions } = options ?? {};
-          return resolveReactComponent(
-            id,
-            path,
-            componentLoaderOptions?.svgrConfig,
-            componentLoaderOptions?.esbuildConfig
-          );
-        }
+      if (query in handlers) {
+        const handler = Reflect.get(handlers, query);
+        return handler({ id, path });
       }
     },
   };
